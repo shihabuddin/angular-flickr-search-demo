7fd8710fbc732c39f6febde8019785b5ea0e25b9
Shihab Uddin <shihab@uddin.com>
2014-10-31 17:05:27 +0100

    search photos from flickr

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,12 @@
 var app = angular.module('FlickrSearch', []);
-app.controller('Main', ['$scope', function ($scope) {
+app.controller('Main', ['$scope', '$http', function ($scope, $http) {
+  var loadPhotos = function (url) {
+    $http.get(url).then(function (response) {
+      $scope.photos = response.data.photos.photo;
+    });
+  };
+
+  var apiKey = '5fb5b14bb0f5ef69ce155577c1f46a6c';
+  var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&tags=bangladesh&format=json&nojsoncallback=1';
+  loadPhotos(url);
 }]);
