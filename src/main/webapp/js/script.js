6f981b2475382ca76dd0745a058ae7483bcdd9ff
Shihab Uddin <shihab@uddin.com>
2014-11-03 10:29:12 +0100

    load each photo thumbnail

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -2,7 +2,16 @@ var app = angular.module('FlickrSearch', []);
 app.controller('Main', ['$scope', '$http', function ($scope, $http) {
   var loadPhotos = function (url) {
     $http.get(url).then(function (response) {
-      $scope.photos = response.data.photos.photo;
+      var photos = $scope.photos = response.data.photos.photo;
+      for (var i = 0; i < photos.length; i++) {
+        loadPhoto(photos[i]);
+      }
+    });
+  };
+  var loadPhoto = function (photo) {
+    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=' + apiKey + '&photo_id=' + photo.id + '&format=json&nojsoncallback=1';
+    $http.get(url).then(function (response) {
+      photo.thumbnail = response.data.sizes.size[2];
     });
   };
 
